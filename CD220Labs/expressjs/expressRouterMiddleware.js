3ad6f2432e94287f880c035808df97da9fcb70b3
Kostek <ksytnik73kwer@gmail.com>
2024-05-02 15:19:34 +0200

    Update expressjs files with improved code readability

--- CD220Labs/expressjs/expressRouterMiddleware.js
+++ CD220Labs/expressjs/expressRouterMiddleware.js
@@ -1,29 +1,28 @@
-const express = require('express');
+const express = require("express");
 const app = new express();
 
-var userRouter = express.Router()
-var itemRouter = express.Router()
+var userRouter = express.Router();
+var itemRouter = express.Router();
 
-app.use("/item",itemRouter)
-app.use("/user",userRouter)
+app.use("/item", itemRouter);
+app.use("/user", userRouter);
 
-userRouter.get("/about/:id",(req,res)=>{
-    res.send("Response about user "+req.params.id)
-})
+userRouter.get("/about/:id", (req, res) => {
+    res.send("Response about user " + req.params.id);
+});
 
-userRouter.get("/details/:id",(req,res)=>{
-    res.send("Details about user "+req.params.id)
-})
+userRouter.get("/details/:id", (req, res) => {
+    res.send("Details about user " + req.params.id);
+});
 
-itemRouter.get("/about/:id",(req,res)=>{
-    res.send("Information about item "+req.params.id)
-})
+itemRouter.get("/about/:id", (req, res) => {
+    res.send("Information about item " + req.params.id);
+});
 
-itemRouter.get("/details/:id",(req,res)=>{
-    res.send("Details about item "+req.params.id)
-})
+itemRouter.get("/details/:id", (req, res) => {
+    res.send("Details about item " + req.params.id);
+});
 
 app.listen(3333, () => {
-    console.log(`Listening at http://localhost:3333`)
-})
-
+    console.log(`Listening at http://localhost:3333`);
+});
--- CD220Labs/expressjs/expressUserDefinedMiddleware.js
+++ CD220Labs/expressjs/expressUserDefinedMiddleware.js
@@ -1,18 +1,17 @@
-const express = require('express');
+const express = require("express");
 const app = new express();
 
-function myLogger(req, res, next){
+function myLogger(req, res, next) {
     req.timeReceived = Date();
     next();
-  }
+}
 
-app.use(myLogger)
+app.use(myLogger);
 
-app.get("/",(req,res)=>{
-    res.send("Request received at "+req.timeReceived+" is a success!")
-})
+app.get("/", (req, res) => {
+    res.send("Request received at " + req.timeReceived + " is a success!");
+});
 
 app.listen(3333, () => {
-    console.log(`Listening at http://localhost:3333`)
-})
-
+    console.log(`Listening at http://localhost:3333`);
+});
